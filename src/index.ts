bf4e9faea96a98108b21201dbb137ccd1f97ca08
Kirill Malanyin <kmalanyin@appliedtech.ru>
2022-07-05 21:23:51 +0500

    added postgres db

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,30 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import { Pool } from "pg";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
-app.get("/", (req, res) => {
-  res.send("Quiz server");
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  port: 5432,
+  database: "quiz",
+  password: "admin",
 });
+
+const getUsers = (req: Request, res: Response) => {
+  pool.query("SELECT * FROM users ORDER BY id ASC", (error, results) => {
+    if (error) {
+      console.log(error);
+    }
+    res.status(200).json(results.rows);
+  });
+};
+
+app.get("/", getUsers);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at https://localhost:${port}`);
